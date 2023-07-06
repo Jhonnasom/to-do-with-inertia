82bc61dd1008d5f65216805be767073cc634b838
Jhonna <jhonna.somarriba@gmail.com>
2023-07-06 13:48:33 -0600

    Se realiza la function de index en el controlador

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Inertia\Response;
 
 class ProductController extends Controller
 {
-    //
+    public function index():Response
+    {
+        $products=Product::query()->paginate(10);
+        return inertia('Products/Index',[
+            'products'=>ProductResource::collection($products),
+        ]);
+    }
 }
