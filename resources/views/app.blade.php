1473e58b330011738e907030395139d8bc9df3a4
Jhonna <jhonna.somarriba@gmail.com>
2023-07-06 12:38:52 -0600

    Anexamos en el head @vitereacrefresh, @vite('src/main.jsx') y @inertiaHead

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        @vitereactrefresh
+        @vite('src/main.jsx')
         @inertiaHead
     </head>
     <body>
