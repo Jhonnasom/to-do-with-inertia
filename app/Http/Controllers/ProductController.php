5a7c0195774659f61388818a97e8851f7e360cc1
Jhonna <jhonna.somarriba@gmail.com>
2023-07-06 21:59:55 -0600

    Se crean las funciones del CRUD en el product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Response;
 
@@ -16,5 +19,30 @@ class ProductController extends Controller
         return inertia('Products/Index',[
             'products'=>ProductResource::collection($products),
         ]);
+    }
+
+    public function create():Response
+    {
+        return inertia('Products/Create');
+    }
+
+    public function store(StoreProductRequest $request):RedirectResponse
+    {
+        $data=$request->validated();
+        Product::create($request->validated());
+        return redirect()->action([static ::class,'index']);
+    }
+
+    public function edit(Product $product):Response
+    {
+        return inertia('Products/Edit',[
+            'product'=>$product,
+        ]);
+    }
+
+    public function update(UpdateProductRequest $request, Product $product):RedirectResponse
+    {
+        $product->update($request->validated());
+        return redirect()->action([static ::class,'index']);
     }
 }
