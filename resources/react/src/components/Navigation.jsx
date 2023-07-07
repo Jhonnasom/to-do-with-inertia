e27976500a47221d08415ee29b0536f6b2cfa099
Jhonna <jhonna.somarriba@gmail.com>
2023-07-06 23:00:32 -0600

    Se crearon estilos para navigation and navigation Link

--- resources/react/src/components/Navigation.jsx
+++ resources/react/src/components/Navigation.jsx
@@ -1,3 +1,10 @@
+import NavigationLink from "./NavigationLink.jsx";
 export default function Navigation(){
-    return <div>Navigation</div>
+    return(
+        <div className="w-[320px] flex flex-col gap-y-4 py-6 px-4">
+            <NavigationLink href="/products">Products</NavigationLink>
+            {/*<NavigationLink href="/todos">Todos</NavigationLink>*/}
+        </div>
+    )
+
 }
--- resources/react/src/components/NavigationLink.jsx
+++ resources/react/src/components/NavigationLink.jsx
@@ -2,6 +2,7 @@ import { Link ,usePage} from "@inertiajs/react";
 export default function NavigationLink(props){
     const page= usePage()
     return(
-        <Link {...props} className="text-gray-500 data-[active]:text-gray-700 font-semibold" data-active={page.url.includes(props.href)?true:undefined}/>
+        <Link {...props} className="text-gray-500 data-[active]:text-gray-700 font-semibold"
+              data-active={page.url.includes(props.href)?true:undefined}/>
     )
 }
