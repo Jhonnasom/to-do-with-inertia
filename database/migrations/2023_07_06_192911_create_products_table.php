b95923760322f177c62fb1bf9646fec86ec994c0
Jhonna <jhonna.somarriba@gmail.com>
2023-07-06 22:44:50 -0600

    Se instalo el paquete de fontawesome para utilizar iconos

--- database/migrations/2023_07_06_192911_create_products_table.php
+++ database/migrations/2023_07_06_192911_create_products_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->string('name');
             $table->decimal('price',6,2);
-
             $table->timestamps();
         });
     }
